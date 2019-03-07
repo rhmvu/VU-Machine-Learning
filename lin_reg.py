7371575d7cf64fbf901bc54e0aca11cd5bd5fa7b
Lucas Faijdherbe <lucasfaijdherbe@MacBook-Pro-van-Lucas.local>
2019-03-07 16:47:56 +0100

    Added nice residual plot for lin_reg.py

--- lin_reg.py
+++ lin_reg.py
@@ -5,6 +5,7 @@ import matplotlib
 
 import matplotlib.pyplot as plt
 from scipy.stats import skew
+import seaborn as sns
 
 from sklearn.linear_model import Ridge, LassoCV
 from sklearn.model_selection import cross_val_score
@@ -14,8 +15,9 @@ from sklearn.model_selection import cross_val_score
 plt.close("all")
 train = pd.read_csv("./data/train.csv")
 test = pd.read_csv("./data/test.csv")
-all_data = pd.concat((train.loc[:,'MSSubClass':'SaleCondition'],
-                      test.loc[:,'MSSubClass':'SaleCondition']))
+all_data = pd.concat((train.loc[:, 'MSSubClass':'SaleCondition'],
+                      test.loc[:, 'MSSubClass':'SaleCondition']))
+
 train.head()
 matplotlib.rcParams['figure.figsize'] = (24.0, 12.0)
 
@@ -23,7 +25,7 @@ train["SalePrice"] = np.log1p(train["SalePrice"])
 
 numeric_feats = all_data.dtypes[all_data.dtypes != "object"].index
 
-skewed_feats = train[numeric_feats].apply(lambda x: skew(x.dropna())) # compute skewness
+skewed_feats = train[numeric_feats].apply(lambda x: skew(x.dropna()))
 skewed_feats = skewed_feats[skewed_feats > 0.75]
 skewed_feats = skewed_feats.index
 
@@ -32,36 +34,40 @@ all_data[skewed_feats] = np.log1p(all_data[skewed_feats])
 all_data = pd.get_dummies(all_data)
 all_data = all_data.fillna(all_data.mean())
 
-X_train = all_data[:train.shape[0]]
-X_test = all_data[train.shape[0]:]
-y = train.SalePrice
+
+X_train = all_data[0:1000]
+y_train = train[0:1000].SalePrice
+
+X_test = all_data[1000:1460]
+y_test = train[1000:1460].SalePrice
 
 
 # Root mean square error
 def rmse_cv(model):
-    rmse = np.sqrt(-cross_val_score(model, X_train, y, scoring="neg_mean_squared_error", cv=5))
+    rmse = np.sqrt(-cross_val_score(model, X_train, y_train, scoring="neg_mean_squared_error", cv=5))
     return rmse
 
-
-model_ridge = Ridge()
-
-# Testing the right alpha value for our model
-alphas = [0.05, 0.1, 0.3, 1, 3, 5, 10, 15, 30, 50, 75]
-cv_ridge = [rmse_cv(Ridge(alpha=alpha)).mean()
-            for alpha in alphas]
-
-
 # Trying L1 regularization
-model_lasso = LassoCV(alphas=[1, 0.1, 0.001, 0.0005]).fit(X_train, y)
+model_lasso = LassoCV(alphas=[1, 0.1, 0.001, 0.0005]).fit(X_train, y_train)
 rmse_cv(model_lasso).mean()
 
 # Lasso gives us an alpha of 0.1231, picks some coefficients and gives the rest a 0 value
 coef = pd.Series(model_lasso.coef_, index=X_train.columns)
 
 # Plotting Residuals
-matplotlib.rcParams['figure.figsize'] = (6.0, 6.0)
 
-preds = pd.DataFrame({"preds":model_lasso.predict(X_train), "true": y})
-preds["residuals"] = preds["true"] - preds["preds"]
-preds.plot(x="preds", y="residuals", kind="scatter")
+plt.scatter(model_lasso.predict(X_train), model_lasso.predict(X_train) - y_train,
+            color="green", s=10, label='Train data')
+
+plt.scatter(model_lasso.predict(X_test), model_lasso.predict(X_test) - y_test,
+            color="blue", s=10, label='Test data')
+
+plt.hlines(y=0, xmin=10, xmax=14, linewidth=2)
+
+plt.legend(loc='upper right')
+
+plt.title("Residual errors")
+
 print(plt.show())
+
+print('Variance score: {}'.format(model_lasso.score(X_test, y_test)))
--- main.py
+++ main.py
@@ -13,10 +13,10 @@ train = pd.read_csv("./data/train.csv")
 test = pd.read_csv("./data/test.csv")
 all_data = train.loc[:, 'MSSubClass':'SaleCondition']
 
-#log transform skewed numeric features:
+# log transform skewed numeric features:
 numeric_feats = all_data.dtypes[all_data.dtypes != "object"].index
 
-skewed_feats = train[numeric_feats].apply(lambda x: skew(x.dropna())) #compute skewness
+skewed_feats = train[numeric_feats].apply(lambda x: skew(x.dropna()))
 skewed_feats = skewed_feats[skewed_feats > 0.75]
 skewed_feats = skewed_feats.index
 
@@ -41,32 +41,30 @@ print('Coefficients: \n', reg.coef_)
   
 # variance score: 1 means perfect prediction 
 print('Variance score: {}'.format(reg.score(X_test, y_test)))
-
-print("test", reg.predict(y_train))
   
 # plot for residual error 
   
-## setting plot style 
+# setting plot style
 plt.style.use('fivethirtyeight') 
   
-## plotting residual errors in training data 
+# plotting residual errors in training data
 plt.scatter(reg.predict(X_train), reg.predict(X_train) - y_train, 
             color = "green", s = 10, label = 'Train data') 
   
-## plotting residual errors in test data 
+# plotting residual errors in test data
 plt.scatter(reg.predict(X_test), reg.predict(X_test) - y_test, 
             color = "blue", s = 10, label = 'Test data') 
   
-## plotting line for zero residual error 
+# plotting line for zero residual error
 plt.hlines(y = 0, xmin = 10, xmax = 14, linewidth = 2) 
   
-## plotting legend 
+# plotting legend
 plt.legend(loc = 'upper right') 
   
-## plot title 
+# plot title
 plt.title("Residual errors") 
   
-## function to show plot 
+# function to show plot
 print(plt.show())
 
 
