af019ee9984fd7f12740a5d4e72eecc7f30b2fb8
rubenvdham <ravdham@protonmail.com>
2019-03-09 23:55:42 +0100

    ~30% performance improvement due to multi-processing in __main__.py

--- __main__.py
+++ __main__.py
@@ -1,5 +1,20 @@
-import svm, lin_reg, basic_lin_reg
+import svm
+import lin_reg
+import basic_lin_reg
+from multiprocessing import Process
 
-print('Basic Linear Regression Model\n\t Variance score: {}\n'.format(basic_lin_reg.run()))
-print('Advanced Linear Regression Model\n\t Variance score: {}\n'.format(lin_reg.run()))
-print('Support Vector Machine (regression)\n\t Variance score: {}'.format(svm.run()))
+model = {
+    "Basic Linear Regression Model": basic_lin_reg,
+    "Advanced Linear Regression Model": lin_reg,
+    "Support Vector Machine (regression)": svm
+}
+
+
+def run_model(model_name, model):
+    print('{}\n\t Variance score: {}\n'.format(model_name, model.run()))
+
+
+if __name__ == '__main__':
+    for name in model:
+        p = Process(target=run_model, args=(name, model[name]))
+        p.start()
